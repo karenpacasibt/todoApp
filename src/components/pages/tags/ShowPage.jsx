dcf09a570e69745a8e7b8e52ce831acf53eb00a7
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-05-08 14:42:06 -0400

    update view tag

--- src/components/pages/tags/ShowPage.jsx
+++ src/components/pages/tags/ShowPage.jsx
@@ -1,33 +1,37 @@
 import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import TagService from '@services/tagService';
 
 export default function ShowPage() {
     const { id } = useParams();
     const [tag, setTag] = useState(null);
     const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!id) return;
         TagService.get(id)
-            .then(({data}) => {
+            .then(({ data }) => {
                 setTag(data);
                 setLoading(false);
             })
             .catch(error => {
                 console.error('Error al cargar el tag:', error);
                 setLoading(false);
+                if (error.response && error.response.status === 404) {
+                    navigate('/tag');
+                }
             });
-    }, [id]);
-    if (loading) return <p className="text-center mt-5">Cargando...</p>;  
-    
+    }, [id, navigate]);
+    if (loading) return <p className="text-center mt-5">Cargando...</p>;
+
     return (
         <div className="w-75 m-auto mt-5 bg-white p-5 rounded shadow">
             <h3 className="text-center mb-4">Información de la Etiqueta</h3>
             <p><strong>ID:</strong> {tag.id}</p>
             <p><strong>Nombre:</strong> {tag.name}</p>
-            
+
             <div className="text-center mt-4">
                 <Link to="/tag/">
                     <Button variant="primary">Atrás</Button>
