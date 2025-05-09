dc7b281febb0a9a60f9196e0cc7c0eb7fddaf8fe
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-05-09 10:56:45 -0400

    Tag service

---
+++ src/services/categoryService.js
@@ -0,0 +1,32 @@
+import api from './api';
+
+const CATEGORY_ROUTE = 'category';
+
+class CategoryService {
+    async getAll() {
+        const { data } = await api.get(CATEGORY_ROUTE);
+        return data;
+    }
+    
+    async get(id) {
+        const { data } = await api.get(`${CATEGORY_ROUTE}/${id}`);
+        return data;
+    }
+
+    async store(params) {
+        const { data } = await api.post(CATEGORY_ROUTE, params);
+        return data;
+    }
+
+    async update(params, id) {
+        const { data } = await api.put(`${CATEGORY_ROUTE}/${id}`, params);
+        return data;
+    }
+
+    async delete(id) {
+        const { data } = await api.delete(`${CATEGORY_ROUTE}/${id}`);
+        return data;
+    }
+}
+
+export default new CategoryService();
