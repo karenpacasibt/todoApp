000e5e18fb3471c62be45a819dc20703a9867be3
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-05-09 12:40:02 -0400

    Delete category

--- src/components/pages/categories/ListPage.jsx
+++ src/components/pages/categories/ListPage.jsx
@@ -3,10 +3,12 @@ import CategoryService from '@services/categoryService';
 import { Button, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Paginate from '../../layouts/Paginate';
+import DeleteModal from './DeleteModal';
 
 function Category() {
     const [categories, setCategories] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [categoryToDelete, setCategoryToDelete] = useState(null);
     const categoriesPerPage = 8;
 
     useEffect(() => {
@@ -24,6 +26,25 @@ function Category() {
         setCurrentPage(pageNumber);
     };
 
+    const handleOpenModal = (category) => {
+        setCategoryToDelete(category);
+    };
+
+    const handleCloseModal = () => {
+        setCategoryToDelete(null);
+    };
+
+    const handleConfirmDelete = async () => {
+        if (!categoryToDelete) return;
+        try {
+            await CategoryService.delete(categoryToDelete.id);
+            handleCloseModal();
+            setCategories(prev => prev.filter(category => category.id !== categoryToDelete.id));
+
+        } catch (error) {
+            console.error('Error al eliminar la etiqueta:', error);
+        }
+    };
     return (
         <div className='w-75 m-auto mt-5 bg-white d-flex justify-content-center flex-column p-5'>
             <Link to="/category/create">
@@ -50,7 +71,7 @@ function Category() {
                                 <Link to={`/category/${category.id}`}>
                                     <Button size="sm" variant="primary" className="me-2"><i className="bi bi-eye-fill"></i></Button>
                                 </Link>
-                                <Button size="sm" variant="danger"><i className="bi bi-trash-fill"></i></Button>
+                                <Button size="sm" variant="danger" onClick={() => handleOpenModal(category)}><i className="bi bi-trash-fill"></i></Button>
                             </td>
                         </tr>
                     ))}
@@ -60,6 +81,12 @@ function Category() {
                 currentPage={currentPage}
                 totalPages={totalPages}
                 onPageChange={handlePageChange}
+            />
+            <DeleteModal
+                show={!!categoryToDelete}
+                onClose={handleCloseModal}
+                onConfirm={handleConfirmDelete}
+                category={categoryToDelete}
             />
         </div>
     );
